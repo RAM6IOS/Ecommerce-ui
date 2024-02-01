7c261a1ea2c75e3cd34719ac24983adbaa406f48
RAM6IOS <ios191169@gmail.com>
2024-02-01 12:29:52 -0800

    creat

---
+++ lib/Data/Model/onboardingModel.dart
@@ -0,0 +1,13 @@
+class OnboardingModel {
+  
+  final String title;
+  final String image ;
+  final String message;
+
+OnboardingModel({
+  required this.title,
+  required this.image,
+  required this.message
+});
+
+}
---
+++ lib/Data/dataSource/Static/onboardingData.dart
@@ -0,0 +1,9 @@
+import 'package:ecommerce_ui/Data/Model/onboardingModel.dart';
+
+List<OnboardingModel> onboarding = [
+  OnboardingModel(title: "Choose Product ", image: "images/onboardingone.png", message: "We Have a 100k Product , Choose \n Your Product From  Our \n E-commerce Shop"),
+  OnboardingModel(title: "Easy And Safe Paymnet", image: "images/onboardingtwo.png", message: "We Have a 100k Product , Choose \n Your Product From Our E-commerce Shop"),
+  OnboardingModel(title: "Track Your Order", image: "images/onboardingthree.png", message: "We Have a 100k Product , Choose \n Your Product From Our E-commerce Shop"),
+  OnboardingModel(title: "Fast Delivery", image: "images/onboardingfour.png", message: "We Have a 100k Product , Choose \n Your Product From Our E-commerce Shop"),
+
+];
