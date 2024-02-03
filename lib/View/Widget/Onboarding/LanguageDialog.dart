c3102e5758db1346d676c0dd6eb778af1f3b6df5
RAM6IOS <ios191169@gmail.com>
2024-02-03 13:35:38 -0800

    creat

---
+++ lib/View/Widget/Onboarding/LanguageDialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LanguageDialog extends StatelessWidget {
+const LanguageDialog({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
