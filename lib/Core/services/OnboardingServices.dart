c3d48dee9021f6940e4334450c0f4f77ed23d765
RAM6IOS <ios191169@gmail.com>
2024-02-03 13:36:15 -0800

    creat

--- lib/Core/services/OnboardingServices.dart
+++ lib/Core/services/OnboardingServices.dart
@@ -1,13 +1,16 @@
+import 'package:ecommerce_ui/Core/localization/localeControler.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MyServices extends GetxService {
   
+  //late SharedPreferences? sharedPreferences ; 
   late SharedPreferences sharedPreferences ; 
-
   Future<MyServices> init() async {
-  sharedPreferences =   await SharedPreferences.getInstance() ; 
-  return this ; 
+    sharedPreferences = await SharedPreferences.getInstance();
+   
+    return this;
   }
 
 }
