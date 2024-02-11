be9c4cc366994d4dee2dcd956436067acc26aeba
RAM6IOS <ios191169@gmail.com>
2024-02-11 12:30:07 -0800

    update

--- lib/controler/OnboardingControler.dart
+++ lib/controler/OnboardingControler.dart
@@ -1,6 +1,10 @@
 import 'package:ecommerce_ui/Core/localization/localeControler.dart';
+import 'package:ecommerce_ui/Core/services/OnboardingServices.dart';
+import 'package:ecommerce_ui/View/Scren/Auth/SignUp.dart';
+import 'package:ecommerce_ui/View/Scren/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OnboardingControler  extends GetxController {
      // Use it to control scrolling between pages, and to select the current page
@@ -8,14 +12,33 @@ class OnboardingControler  extends GetxController {
   // The current page is tracked
   int currentPage = 0;
 
+  bool showOnboarding = false ;
+
   MyLocaleController localeController = Get.find();
+  MyServices services = Get.find();
 
   @override
   void onInit() {
     super.onInit();
-  
-    // يتم استدعاء هذه الدالة عند إنشاء المتحكم (المكافئ ل initState في StatefulWidget)
-    // يمكنك وضع التكويد الخاص بك هنا.
+    Future.delayed(Duration.zero, () {
+    ShowOnboarding();
+  });
+  }
+
+  void ShowOnboarding() async{
+   if (services.sharedPreferences.getBool("showOnboarding") == true) {
+    showOnboarding = services.sharedPreferences.getBool("showOnboarding")!;
+    if (showOnboarding) {
+      await Get.offAll(() => SignUp());
+    } 
+  }
+
+  }
+  void shangeOnboarding() {
+   showOnboarding = true;
+  services.sharedPreferences.setBool("showOnboarding", showOnboarding);
+  print(services.sharedPreferences.getBool("showOnboarding"));
+    update();
   }
 
   void CurrentPage(int page){
