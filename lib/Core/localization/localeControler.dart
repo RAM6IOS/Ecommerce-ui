5349cc6e9927e0cefc5cb297fa95ea6b34ad00ac
RAM6IOS <ios191169@gmail.com>
2024-02-11 12:30:49 -0800

    update

--- lib/Core/localization/localeControler.dart
+++ lib/Core/localization/localeControler.dart
@@ -1,14 +1,7 @@
 
-
-
-
 import 'package:ecommerce_ui/Core/services/OnboardingServices.dart';
-import 'package:ecommerce_ui/main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-// ignore: depend_on_referenced_packages
-import 'package:flutter_hooks/flutter_hooks.dart';
 
 class MyLocaleController extends GetxController {
 
