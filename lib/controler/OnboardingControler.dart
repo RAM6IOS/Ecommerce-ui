e42ec3819cc8bc896d808ee8edf1e9487fd13c1b
RAM6IOS <ios191169@gmail.com>
2024-03-24 14:19:35 -0700

    update

--- lib/controler/OnboardingControler.dart
+++ lib/controler/OnboardingControler.dart
@@ -1,6 +1,4 @@
-import 'package:ecommerce_ui/Core/localization/localeControler.dart';
 import 'package:ecommerce_ui/Core/services/OnboardingServices.dart';
-import 'package:ecommerce_ui/View/Scren/Auth/SignUp.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,41 +7,29 @@ class OnboardingControler  extends GetxController {
   final PageController pageController = PageController(initialPage: 0);
   // The current page is tracked
   int currentPage = 0;
+
   bool showOnboarding = false ;
 
-  MyLocaleController localeController = Get.find();
+  
   MyServices services = Get.find();
 
   @override
   void onInit() {
     super.onInit();
-   // Future.delayed(Duration.zero, () {
-  //});
   }
-
-
-/*
-  void ShowOnboarding() async{
-   if (services.sharedPreferences.getBool("showOnboarding") != null ) {
-    showOnboarding = services.sharedPreferences.getBool("showOnboarding")!;
-    if (showOnboarding) {
-      await Get.offAll(() => SignUp());
-    } 
-  }
-
-  }
-  */
-  
-  void shangeOnboarding() {
+  // Function to change onboarding status and navigate to SignUp screen
+  void changeOnboarding() {
    services.sharedPreferences.setString("role" ,"SignUp");
     Get.offAllNamed("/SignUp");
     update();
   }
 
+// تحديث الصفحة الحالية
   void CurrentPage(int page){
     currentPage = page ;
     update();
   }
+  // التبديل إلى الصفحة التالية
   void NextPage(){
     pageController.nextPage(
       duration: const Duration(milliseconds: 500), 
